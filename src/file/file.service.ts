c62c129cfb14dd6125981061ebee82aacbbf6d9e
PrakritPrad <poom66310@gmail.com>
2026-03-02 10:11:50 +0700

    skibidi123123

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,12 +3,12 @@ import { promises as fs } from 'fs';
 
 @Injectable()
 export class FileService {
-    async readFile(path: string): Promise<string> {
-        return await fs.readFile(path, 'utf8');
-    }
+  async readFile(path: string): Promise<string> {
+    return await fs.readFile(path, 'utf8');
+  }
 
-    async readJson<T>(path: string): Promise<T> {
-        const data = await this.readFile(path);
-        return JSON.parse(data) as T;
-    }
+  async readJson<T>(path: string): Promise<T> {
+    const data = await this.readFile(path);
+    return JSON.parse(data) as T;
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { IUser } from './user.interface'
+import { IUser } from './user.interface';
 import { FileService } from 'src/file/file.service';
 @Injectable()
 export class UserService {
-    private readonly filePath = 'src/data/users.json';
-    constructor(private readonly fileService: FileService) { }
+  private readonly filePath = 'src/data/users.json';
+  constructor(private readonly fileService: FileService) {}
 
-    test(): string[] {
-        return [];
-    }
+  test(): string[] {
+    return [];
+  }
 
-    async findAll(): Promise<IUser[]> {
-        return await this.fileService.readJson<IUser[]>(this.filePath);
-    }
+  async findAll(): Promise<IUser[]> {
+    return await this.fileService.readJson<IUser[]>(this.filePath);
+  }
 }
