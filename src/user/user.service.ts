555b4a2053e8ff7dfccd837468a328465757030e
PrakritPrad <poom66310@gmail.com>
2026-03-02 10:16:37 +0700

    test02

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { IUser } from './user.interface';
-import { FileService } from 'src/file/file.service';
+
 @Injectable()
 export class UserService {
-  private readonly filePath = 'src/data/users.json';
-  constructor(private readonly fileService: FileService) {}
 
-  test(): string[] {
-    return [];
-  }
+    test(): string[] {
+        return [];
+    }
 
-  async findAll(): Promise<IUser[]> {
-    return await this.fileService.readJson<IUser[]>(this.filePath);
-  }
+
 }
