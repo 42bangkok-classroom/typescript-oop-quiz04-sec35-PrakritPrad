c987aabfb337b20d5f64aafd1fb88cd4704d1f17
PrakritPrad <poom66310@gmail.com>
2026-03-02 09:35:29 +0700

    testsetset

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,9 @@
-
 import { Controller, Get } from '@nestjs/common';
-import { UserService } from './user.service' ;
+import { UserService } from './user.service';
 @Controller('user/test')
 export class UserController {
-    constructor(private readonly UserService: UserService) {}
-  
+  constructor(private readonly UserService: UserService) {}
+
   @Get()
   test(): string[] {
     return this.UserService.test();
